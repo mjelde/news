f82a88157db48ebe6b79327fdff9a160229f28da
Titane73 <thomas.ti.hall@gmail.com>
2021-03-11 11:12:37 -0800

    updates winter2021_news.sql for real this time

--- news/winter2021_news.sql
+++ news/winter2021_news.sql
@@ -1,26 +1,43 @@
 -- winter2021_news.sql
 
-SET foreign_key_checks = 0; -- turn off constraints temporarily
-
-DROP TABLE if EXISTS winter2021_feeds;
-DROP TABLE if EXISTS winter2021_feed_categories;
-
+SET
+    foreign_key_checks = 0; -- turn off constraints temporarily
+    
+DROP TABLE IF EXISTS
+    winter2021_feeds;
+    
+DROP TABLE IF EXISTS
+    winter2021_feed_categories;
+    
 CREATE TABLE winter2021_feeds(
-FeedID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-FeedName VARCHAR(255) DEFAULT '',
-FeedDescription TEXT DEFAULT '',
-PRIMARY KEY (FeedID)
-)
+    FeedID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    FeedName VARCHAR(255) DEFAULT '',
+    FeedDescription TEXT DEFAULT '',
+    PRIMARY KEY(FeedID)
+);
 
 CREATE TABLE winter2021_feed_categories(
-FeedCategoryID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-FeedID INT DEFAULT 0;
-CategoryName VARCHAR(255) DEFAULT '',
-CategoryURL TEXT DEFAULT '',
-PRIMARY KEY (FeedCategoryID)
-)
+    FeedCategoryID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    FeedID INT DEFAULT 0,
+    CategoryName VARCHAR(255) DEFAULT '',
+    CategoryURL TEXT DEFAULT '',
+    PRIMARY KEY(FeedCategoryID)
+);
 
-INSERT INTO winter2021_feeds VALUES (NULL,'Test Feed 1','Feed Name 1',);
-INSERT INTO winter2021_feed_categories VALUES (NULL,1,'Test Category1','www.test-category.com');
+INSERT INTO winter2021_feeds
+VALUES(
+    NULL,
+    'Test Feed 1', 
+    'Feed Name 1'
+);
 
-SET foreign_key_checks = 1; -- turn foreign key check back on
+INSERT INTO winter2021_feed_categories
+VALUES(
+    NULL,
+    1,
+    'Test Category1',
+    'www.test-category.com'
+);
+
+SET
+    foreign_key_checks = 1; -- turn foreign key check back on
