229f07c5bbcaf9f9c817d542c81fb81833aea647
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2021-03-13 21:01:53 -0800

    list and category view
    comments out empty css example ruleset to fix error in header_inc
    adds news to the nav menu
    edits tooltip for news in nav
    adds credentials to gitignore
    converts survey index to show the news feeds
    converts survey_view to news_view
    adds Feed class based on Survey class
    adds Category class based on Question class

---
+++ news/news_view.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * news_view.php is a page to demonstrate the proof of concept of the 
+ * initial News objects.
+ *
+ * Objects in this version are the Feed and FeedCategory objects
+ * 
+ * @package News
+ * @author Sharon Alexander, Kira Abell, Ti Hall <group1@example.com>
+ * @version 1.5 2021/03/13
+ * @link http://www.example.com/
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @see Feed.php
+ * @see Category.php
+ */
+ 
+require '../inc_0700/config_inc.php'; # provides configuration, pathing, error handling, db credentials
+spl_autoload_register('MyAutoLoader::NamespaceLoader'); //required to load News namespace objects
+$config->metaRobots = 'no index, no follow'; # never index feed pages
+
+# check variable of item passed in - if invalid data, forcibly redirect back to demo_list.php page
+if(isset($_GET['id']) && (int)$_GET['id'] > 0) { # roper data must be on querystring
+	 $myID = (int)$_GET['id']; # Convert to integer, will equate to zero if fails
+} else {
+	myRedirect(VIRTUAL_PATH . "news/index.php");
+}
+
+$myFeed = new News\Feed($myID); //MY_Feed extends feed class so methods can be added
+if($myFeed->isValid) {
+	$config->titleTag = "$myFeed->FeedName Feed!";
+} else {
+	$config->titleTag = smartTitle(); //use constant 
+}
+
+#END CONFIG AREA ---------------------------------------------------------- 
+
+get_header(); # defaults to theme header or header_inc.php
+?>
+
+
+<h3><?=$myFeed->FeedName;?></h3>
+
+<?php
+
+if($myFeed->isValid) { # check to see if we have a valid FeedID
+	echo '<p>' . $myFeed->FeedDescription . '</p>';
+	echo $myFeed->showCategories();
+} else {
+	echo "Sorry, no such feed!";	
+}
+
+get_footer(); # defaults to theme footer or footer_inc.php
